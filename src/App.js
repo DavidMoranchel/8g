9c20d05e49b9c94db5ef9b08dec356ab5079081f
David Moranchel <david.arcshtag@gmail.com>
2020-08-06 19:43:06 -0500

    switch color component :octocat:

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-// Reactstrap
-import { CardColumns } from "reactstrap";
-
 // Components
-import FunctionGreet from "./components/FunctionGreet";
-import ClassGreet from "./components/ClassGreet";
-import Post from "./components/Post";
-import Clock from "./components/Clock";
+import SwitchExample from "./components/SwitchExample";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <FunctionGreet firstName="Melissa" lastName="Castillo" />
-        <ClassGreet firstName="Vicente" lastName="Muñoz" />
-        <ClassGreet firstName="David" lastName="Moranchel" />
-      </header> */}
-      <Clock />
-
-      {/* 
-        Crear un componente funcional que renderice una Card 
-        haciendo referencia al post de un blog
-        
-        Busquen referencias de blogs y la estructura de sus posts
-      
-        Tips:
-        - Primero abstraigan el objeto Post (esto es tu componente y sus props)
-        - Utilicen reactstrap para que se vea cute!
-      */}
-
-      {/* <CardColumns>
-        <Post
-          title="Amazing card"
-          subtitle="This is the card!"
-          content="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-          image="https://picsum.photos/id/1/100/100"
-          tags={"life,olv"}
-        />
-        <Post
-          title="Amazing card"
-          subtitle="This is the card!"
-          content="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-          image="https://picsum.photos/id/2/100/100"
-          tags={"life,food"}
-        />
-        <Post
-          title="Amazing card"
-          subtitle="This is the card!"
-          content="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-          image="https://picsum.photos/id/3/100/100"
-          tags={"life,food"}
-        />
-      </CardColumns> */}
+      <SwitchExample />
     </div>
   );
 }
