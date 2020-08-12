02e9f7aa2da07d3d101a73954f264f3ef8f0ac79
David Moranchel <david.arcshtag@gmail.com>
2020-08-11 19:51:45 -0500

    add react router dom and route the app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
 import "./App.css";
 
+// RRD
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 // Screens
-import BlogNotes from "./screens/NotesBlog";
+import NotesBlog from "./screens/NotesBlog";
 import CurrencyConverter from "./screens/CurrencyConverter";
 
 function App() {
   return (
-    <div className="App">
-      <BlogNotes />
-      <CurrencyConverter />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact={true} path="/">
+            <>
+              <h1>Home</h1>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/notesBlog">Blog de notas</Link>
+                  </li>
+                  <li>
+                    <Link to="/currencyConverter">Conversor de divisas</Link>
+                  </li>
+                </ul>
+              </nav>
+            </>
+          </Route>
+          <Route path="/notesBlog">
+            <NotesBlog />
+          </Route>
+          <Route path="/currencyConverter">
+            <CurrencyConverter />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
